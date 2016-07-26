5bc36e88717e73ad132ad77b762b2a4c5d58bc7e
homepc <rc.crusader@gmail.com>
2016-07-26 23:50:40 +0300

    Initial release: lamp on/off working via web

---
+++ run_server.py
@@ -0,0 +1,22 @@
+import RPIO
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+RPIO.setup(14, RPIO.OUT)
+@app.route("/on")
+def ledon():
+	RPIO.output(14, True)	
+	return render_template('page.html')
+
+@app.route("/off")
+def ledoff():
+	RPIO.output(14, False)	
+	return render_template('page.html')
+
+@app.route("/")
+def hello():
+	return render_template('page.html')
+
+
+if __name__ == "__main__":
+    app.run()
