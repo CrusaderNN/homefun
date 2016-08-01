485c7d6d1236f9cbf2d124c7ce6729f9c7bd533e
homepc <rc.crusader@gmail.com>
2016-08-01 22:41:25 +0300

    Adding frames, camera frame

--- run_server.py
+++ run_server.py
@@ -1,34 +1,26 @@
-#adding comment
 import RPIO
 from flask import Flask
 from flask import render_template
-
-import shutil
-
-import requests
-
-url = 'http://example.com/img.png'
-response = requests.get(url, stream=True)
-with open('img.png', 'wb') as out_file:
-    shutil.copyfileobj(response.raw, out_file)
-del response
 
 app = Flask(__name__)
 RPIO.setup(14, RPIO.OUT)
 @app.route("/on")
 def ledon():
 	RPIO.output(14, True)	
-	return render_template('page.html')
+	return render_template('buttons.html')
 
 @app.route("/off")
 def ledoff():
 	RPIO.output(14, False)	
-	return render_template('page.html')
+	return render_template('buttons.html')
 
 @app.route("/")
-def hello():
+def hello(): 
 	return render_template('page.html')
 
+@app.route("/buttons")
+def hello():
+	return render_template('buttons.html')
 
 if __name__ == "__main__":
     app.run()
